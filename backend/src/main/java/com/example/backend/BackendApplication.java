5befb4f57daad2524192df489ded09bc8f15c0f3
cogns8571 <cogns8571@naver.com>
2026-01-12 20:47:28 +0900

    git commit -m "feat: configure spring boot active profile in docker-compose"

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,20 +1,14 @@
 package com.example.backend;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
 
 
 @SpringBootApplication
 class BackendApplication {
 	public static void main(String[] args) {
-		// 1. .env 파일 로드
-		Dotenv dotenv = Dotenv.configure().load();
-
-		// 2. 읽어온 값을 시스템 프로퍼티로 등록 (Spring이 ${}로 인식할 수 있게 함)
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
